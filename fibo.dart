56e5c22a5233e6ec8870272960673bf0ae3910cc
Karanbohara01 <karanbohara216@gmail.com>
2024-11-18 21:41:04 +0545

    fibo

---
+++ fibo.dart
@@ -0,0 +1,19 @@
+void main() {
+  int n = 20; // Number of terms in the Fibonacci sequence
+  List<int> fibonacci = generateFibonacci(n);
+
+  print("Fibonacci sequence with $n terms:");
+  print(fibonacci);
+}
+
+List<int> generateFibonacci(int n) {
+  if (n <= 0) return [];
+  if (n == 1) return [0];
+  if (n == 2) return [0, 1];
+
+  List<int> sequence = [0, 1];
+  for (int i = 2; i < n; i++) {
+    sequence.add(sequence[i - 1] + sequence[i - 2]);
+  }
+  return sequence;
+}
